fe90a2169702fba51ad61a705b5095e5bb9b8992
01Eddie <jesus.11.97@hotmail.com>
2020-09-25 19:29:42 -0500

    modified my portfolio 1.0

--- js/inicio.js
+++ js/inicio.js
@@ -1,1 +1,20 @@
-console.log('Hola');
+
+      /* function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+ */
+
+  // selector
+  var menu = document.querySelector('.hamburger');
+
+  // method
+  function toggleMenu (event) {
+    this.classList.toggle('is-active');
+    document.querySelector( ".menuppal" ).classList.toggle("is_active");
+    event.preventDefault();
+  }
+  
+  // event
+  menu.addEventListener('click', toggleMenu, false);
+      
